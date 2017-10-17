cc8d139c62c717a1091ece1f811002a8b198c482
TsubasaTakagi <mizuirotech@gmail.com>
2017-10-17 15:34:26 +0900

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   get '/about' => 'static_pages#about'
   get "/contact" => 'static_pages#contact'
   resources :users
-
+  resources :account_activations, only: [:edit]
 end
